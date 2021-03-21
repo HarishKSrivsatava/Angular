aa4128a61510452c0da70d98ea541b54dd92fcb2
Harish Kumar <harishkumar9758@gmail.com>
2021-03-21 15:39:22 +0530

    recipe and shopping components created

--- angular-app-shoping-world/src/app/app.module.ts
+++ angular-app-shoping-world/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './receipe/recipes-list/recipes-list.component';
+import { RecipesDetailsComponent } from './receipe/recipes-details/recipes-details.component';
+import { RecipeItemComponent } from './receipe/recipes-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesDetailsComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
